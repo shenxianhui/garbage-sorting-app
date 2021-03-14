2c787d9831f4178f43dbc5235b7b5dbb8ac22d78
wells <shenxh0928@qq.com>
2021-03-14 21:30:01 +0800

    feat: 新增表单页

--- src/pages/discriminate/manual-classification/manual-classification.vue
+++ src/pages/discriminate/manual-classification/manual-classification.vue
@@ -2,35 +2,43 @@
  * @Author: shenxh
  * @Date: 2021-03-14 17:25:31
  * @LastEditors: shenxh
- * @LastEditTime: 2021-03-14 19:05:56
+ * @LastEditTime: 2021-03-14 21:21:41
  * @Description: 手动提交分类
 -->
 
 <template>
   <view class="manual-classification">
-    <div class="guide">
-      <image class="robot" src="@/static/img/robot.png" />
-      <div class="btn guide-text">请帮我识别一下</div>
-    </div>
-    <div class="manual-classification-content">
-      <div class="goods-img">
+    <view class="manual-classification-content">
+      <view class="guide">
         <image class="robot" src="@/static/img/robot.png" />
-      </div>
-      <div class="product-name">
-        <u-input v-model="form.name" placeholder="请输入产品名称" />
-      </div>
-      <div class="select-type">
+        <view class="btn guide-text">请帮我识别一下</view>
+      </view>
+      <view class="goods-img">
+        <image mode="aspectFit" src="@/static/img/robot.png" />
+      </view>
+      <view class="product-name">
+        <u-input v-model="form.name" :clearable="false" placeholder="请输入产品名称" />
+      </view>
+      <view class="select-type">
         <select-type class="title">选择分类</select-type>
-        <div class="options">
-          <div class="btn option">可回收垃圾</div>
-          <div class="btn option">有害垃圾</div>
-          <div class="btn option">厨余垃圾</div>
-          <div class="btn option">干垃圾</div>
-        </div>
-      </div>
-      <div class="remind">注: 不知道哪一类, 可以不选择分类</div>
-      <div class="btn">提交</div>
-    </div>
+        <view class="options">
+          <view class="btn option" :class="{ active: form.type == 1 }" @click="handleOpt(1)">
+            可回收垃圾
+          </view>
+          <view class="btn option" :class="{ active: form.type == 2 }" @click="handleOpt(2)">
+            有害垃圾
+          </view>
+          <view class="btn option" :class="{ active: form.type == 3 }" @click="handleOpt(3)">
+            厨余垃圾
+          </view>
+          <view class="btn option" :class="{ active: form.type == 4 }" @click="handleOpt(4)">
+            干垃圾
+          </view>
+        </view>
+      </view>
+      <view class="remind">注: 不知道哪一类, 可以不选择分类</view>
+      <view class="btn" @click="handleSubForm()">提交</view>
+    </view>
   </view>
 </template>
 
@@ -41,7 +49,10 @@ export default {
   props: {},
   data() {
     return {
-      form: {}
+      form: {
+        name: '',
+        type: null
+      }
     };
   },
   computed: {},
@@ -51,8 +62,124 @@ export default {
   onReady() {},
   onHide() {},
   onUnload() {},
-  methods: {}
+  methods: {
+    handleOpt(type) {
+      if (this.form.type == type) {
+        this.form.type = null;
+      } else {
+        this.form.type = type;
+      }
+    },
+    handleSubForm() {
+      if (!this.form.name) {
+        uni.showToast({
+          title: '请输入产品名称',
+          icon: 'none',
+          duration: 2000
+        });
+
+        return;
+      }
+
+      uni.showToast({
+        title: '提交成功',
+        icon: 'success',
+        duration: 2000
+      });
+
+      console.log(this.form);
+      uni.redirectTo({
+        url: `/pages/discriminate/my-card/my-card`
+      });
+    }
+  }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.manual-classification {
+  background-color: #278c53;
+  height: 100%;
+  .manual-classification-content {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-radius: 30rpx 30rpx 0 0;
+    background-color: #fff;
+    padding: 60rpx 80rpx;
+    .btn {
+      text-align: center;
+      width: 100%;
+      height: 70rpx;
+      background-color: #278c53;
+      border-radius: 35rpx;
+      font-size: 30rpx;
+      line-height: 70rpx;
+      color: #fff;
+    }
+    .guide {
+      position: absolute;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      top: -146rpx;
+      left: 50%;
+      transform: translate(-50%, 0);
+      .robot {
+        width: 200rpx;
+        height: 160rpx;
+      }
+      .guide-text {
+        width: 360rpx;
+      }
+    }
+    .goods-img {
+      width: 100%;
+      height: 300rpx;
+      border-radius: 20rpx;
+      border: 1px solid #278c53;
+      overflow: hidden;
+      image {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .product-name {
+      margin-top: 20px;
+      /deep/ input {
+        height: 100%;
+        background: #eee;
+        border-radius: 40rpx;
+        padding: 0 40rpx;
+      }
+    }
+    .select-type {
+      margin-top: 50rpx;
+      .title {
+        color: #666;
+        font-size: 40rpx;
+        font-weight: bold;
+      }
+      .options {
+        .option {
+          margin-top: 30rpx;
+          background: #eee;
+          color: #999;
+          &.active {
+            background: #278c53;
+            color: #fff;
+          }
+        }
+      }
+    }
+    .remind {
+      font-size: 26rpx;
+      color: #666;
+      margin-top: 50rpx;
+    }
+    .btn {
+      margin-top: 20rpx;
+    }
+  }
+}
+</style>
---
+++ src/pages/discriminate/my-card/my-card.vue
@@ -0,0 +1,47 @@
+<!--
+ * @Author: shenxh
+ * @Date: 2021-03-14 17:25:31
+ * @LastEditors: shenxh
+ * @LastEditTime: 2021-03-14 21:20:42
+ * @Description: 我的卡片
+-->
+
+<template>
+  <view class="my-card">
+    <view class="my-card-content"> </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'my-card',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  onLoad() {},
+  onShow() {},
+  onReady() {},
+  onHide() {},
+  onUnload() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+.my-card {
+  background-color: #278c53;
+  height: 100%;
+  .my-card-content {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-radius: 30rpx 30rpx 0 0;
+    background-color: #fff;
+    padding: 60rpx 80rpx;
+  }
+}
+</style>
